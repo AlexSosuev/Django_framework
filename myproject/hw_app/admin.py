7f628506503a410aa9d7cc6bd6634bcec79c12b7
aleksandr sosuev <sav_ml@mail.ru>
2024-04-12 06:42:38 +0500

    added hw_5

--- myproject/hw_app/admin.py
+++ myproject/hw_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Client, Product, OrderItem, Order
 
-# Register your models here.
+
+class HWAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity', 'added_date']
+    ordering = ['name', '-price']
+    list_filter = ['added_date', 'price']
+    search_fields = ['description']
+    search_help_text = 'Поиск по полю Описание продукта (description)'
+
+admin.site.register(Client)
+admin.site.register(Product, HWAdmin)
+admin.site.register(OrderItem)
+admin.site.register(Order)
